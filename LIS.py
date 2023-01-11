c8a223c2d3bf78521312070a581d5707c365a48f
amkatayama <113309314+amkatayama@users.noreply.github.com>
2023-01-11 15:20:58 -0700

    Update LIS.py

--- LIS.py
+++ LIS.py
@@ -4,15 +4,21 @@ class Solution:
         sol = [0] * len(nums)
 
         for i in range(len(nums)):
-            s = i  # starting point 
-            length = 1  # initializing length of LIS
-            for j in range(i+1, len(nums)):
+            l = i  # left pointer 
+            r = len(nums)-i-1  # right pointer 
+            len_l, len_r = 1, 1 # length of LIS from the left and right 
+            for j in range(r):
+                # increase the length of LIS, and update starting point if the subsequent element is larger
+                if nums[j+1] > nums[l]:
+                    # shift the left pointer
+                    l = j+1
+                    len_l += 1
+                if nums[len(nums)-j-2] < nums[r]:
+                    # shift the right pointer
+                    r = len(nums)-j-2
+                    len_r += 1
                 # if the subsequent integer is smaller then do nothing
-                if nums[j] <= nums[s]:
-                    continue
-                # increase the length of LIS, and update starting point if the subsequent element is larger 
-                length += 1 
-                s = j
-            sol[i] = length
+ 
+            sol[i] = max(len_l, len_r)
         
         return max(sol)
